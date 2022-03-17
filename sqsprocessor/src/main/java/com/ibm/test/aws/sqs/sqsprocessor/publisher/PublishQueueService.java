9f03347974f6973f566caa59c56bce09495bb1fb
gapal1974@gmail.com <gapal1974@gmail.com>
2022-03-17 17:07:05 +0530

    commit change for upgrade POM

--- sqsprocessor/src/main/java/com/ibm/test/aws/sqs/sqsprocessor/publisher/PublishQueueService.java
+++ sqsprocessor/src/main/java/com/ibm/test/aws/sqs/sqsprocessor/publisher/PublishQueueService.java
@@ -7,13 +7,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import lombok.RequiredArgsConstructor;
 
 /**
