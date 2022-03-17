60fb1fa0eec6228d07d2cfa03ded475cd8d89f37
gapal1974@gmail.com <gapal1974@gmail.com>
2022-03-17 18:39:31 +0530

    commit2

--- sqsprocessor/src/main/java/com/ibm/test/aws/sqs/sqsprocessor/custom/exception/ApiError.java
+++ sqsprocessor/src/main/java/com/ibm/test/aws/sqs/sqsprocessor/custom/exception/ApiError.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
  * 
  */
@@ -37,43 +37,4 @@ public class ApiError {
         errors = Arrays.asList(error);
     }
 }
-=======
-/**
- * 
- */
-package com.ibm.test.aws.sqs.sqsprocessor.custom.exception;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-
-import lombok.Data;
-
-/**
- * @author 02029H744
- *
- *
- */
-@Data
-public class ApiError {
-
-    private HttpStatus status;
-    private String message;
-    private List<String> errors;
-
-    public ApiError(HttpStatus status, String message, List<String> errors) {
-        super();
-        this.status = status;
-        this.message = message;
-        this.errors = errors;
-    }
-
-    public ApiError(HttpStatus status, String message, String error) {
-        super();
-        this.status = status;
-        this.message = message;
-        errors = Arrays.asList(error);
-    }
-}
->>>>>>> branch 'master' of https://github.com/gapal74/springexperiments.git
