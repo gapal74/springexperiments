7c07e47b7a92d117b00b24111c2b49bd183fda6c
gapal1974@gmail.com <gapal1974@gmail.com>
2022-03-17 23:30:44 +0530

    added attrubutes and Message Attributes

--- sqsprocessor/src/main/java/com/ibm/test/aws/sqs/sqsprocessor/publisher/PublishQueueService.java
+++ sqsprocessor/src/main/java/com/ibm/test/aws/sqs/sqsprocessor/publisher/PublishQueueService.java
@@ -4,7 +4,9 @@
  */
 package com.ibm.test.aws.sqs.sqsprocessor.publisher;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
@@ -37,7 +41,18 @@ public class PublishQueueService {
 			ObjectMapper mapper = new ObjectMapper();
 			String jsonString = mapper.writeValueAsString(messageMap);
 			System.out.println("In building Service"+jsonString);
-			queueMessagingTemplate.send(endPoint, MessageBuilder.withPayload(jsonString).build());
+			MessageAttributeValue traceIdValue = new MessageAttributeValue();
+			MessageAttributeValue operationTypeValue = new MessageAttributeValue();
+			Map<String, String> attributeMap= new HashMap<String,String>();
+			Message msg = new Message();
+			// generates a UUID as the traceId
+			msg.addMessageAttributesEntry("TRACE_ID", traceIdValue.withDataType("String").withStringValue(UUID.randomUUID().toString()));
+			msg.addMessageAttributesEntry("OPERATION_TYPE", operationTypeValue.withDataType("String").withStringValue("DELiVER"));
+			attributeMap.put("SOURCE_DOMAIN", "ORCH");
+			attributeMap.put("DESTINATION_DOMAIN", "TASK");
+			msg.setBody(jsonString);
+			msg.setAttributes(attributeMap);
+			queueMessagingTemplate.send(endPoint, MessageBuilder.withPayload(msg).build());
 			// logger.info("Message sent successfully " + jsonString);
 		} catch (Exception e) {
 			e.printStackTrace();
